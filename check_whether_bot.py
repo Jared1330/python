5e034de7a3af80e7620e1a23fa56f60ece8beb0c
Jared1330 <37220430+Jared1330@users.noreply.github.com>
2019-10-03 11:13:04 +0300

    Update check_whether_bot.py

--- check_whether_bot.py
+++ check_whether_bot.py
@@ -12,7 +12,7 @@ def start_message(message):
         owm = pyowm.OWM('fa5d4c47bfc6e60d075e8401154fb179')
         observation = owm.weather_at_place('Kiev')
         w = observation.get_weather()
-        a= str(("Вера, привет, сейчас в Дндз +" +str(round(w.get_temperature('celsius')['temp'])) + " градусов по Цельсию."))
+        a= str(("Cейчас в Киеве +" +str(round(w.get_temperature('celsius')['temp'])) + " градусов по Цельсию."))
         res = str(a)
         bot.send_message(message.chat.id,  res )
         bot.send_sticker(message.chat.id, 'CAADAgAD5gQAAjaQ6wPuj1YgnhracwI')
