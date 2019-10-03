d018b2f0a2034422d4ce9168af5841f1f7688655
Jared1330 <37220430+Jared1330@users.noreply.github.com>
2019-10-03 16:08:02 +0300

    Update check_whether_bot.py

--- check_whether_bot.py
+++ check_whether_bot.py
@@ -1,3 +1,5 @@
+
+
 # -*- coding: utf-8 -*-
 # t.me/tetriswefdr4231wedfrt543_bot
 
@@ -12,10 +14,25 @@ def start_message(message):
         owm = pyowm.OWM('fa5d4c47bfc6e60d075e8401154fb179')
         observation = owm.weather_at_place('Kiev')
         w = observation.get_weather()
-        a= str(("Cейчас в Киеве +" +str(round(w.get_temperature('celsius')['temp'])) + " градусов по Цельсию."))
+        a= str(("C     ^g   ^a     ^z         +" +str(round(w.get_temperature('celsius')['temp'])) + "    ^`     ^c ^a                 ^l ^a   ^n."))
         res = str(a)
         bot.send_message(message.chat.id,  res )
         bot.send_sticker(message.chat.id, 'CAADAgAD5gQAAjaQ6wPuj1YgnhracwI')
+        log(message)
+
+
+def log(message):
+    my_file = open("log.txt", "a")
+    my_file.write("<!------!>\n")
+    from datetime import datetime
+    my_file.close()
+    print("<!------!>")
+    from datetime import datetime
+    print(datetime.now())
+    print("         ^i            ^b {0} {1} (id = {2}) \n {3}".format(message.from_user.first_name,
+                                                              message.from_user.last_name,
+                                                              str(message.from_user.id), message.text))
+
 
 bot.polling()
 
