9d7e55834141aebdc9dd748434aa70aa5f93bd58
Jared1330 <37220430+Jared1330@users.noreply.github.com>
2019-10-18 17:11:30 +0300

    Update check_whether_bot.py

--- check_whether_bot.py
+++ check_whether_bot.py
@@ -1,20 +1,18 @@
-
-
 # -*- coding: utf-8 -*-
-# t.me/tetriswefdr4231wedfrt543_bot
 
 import telebot
 import pyowm
+from datetime import datetime
 
 bot = telebot.TeleBot('831420068:AAGWx-iA2C6QP5qmxclIxhhom93N29d61c0')
-@bot.message_handler(commands=['start'])
 
+@bot.message_handler(commands=['start'])
 def start_message(message):
         res = str(" ")
         owm = pyowm.OWM('fa5d4c47bfc6e60d075e8401154fb179')
         observation = owm.weather_at_place('Kiev')
         w = observation.get_weather()
-        a= str(("C     ^g   ^a     ^z         +" +str(round(w.get_temperature('celsius')['temp'])) + "    ^`     ^c ^a                 ^l ^a   ^n."))
+        a= str(("Today in Kyiv is " +str(round(w.get_temperature('celsius')['temp']))))
         res = str(a)
         bot.send_message(message.chat.id,  res )
         bot.send_sticker(message.chat.id, 'CAADAgAD5gQAAjaQ6wPuj1YgnhracwI')
@@ -24,15 +22,10 @@ def start_message(message):
 def log(message):
     my_file = open("log.txt", "a")
     my_file.write("<!------!>\n")
-    from datetime import datetime
+    date = str(datetime.now())
+    my_file.write(date + '\n')
+    my_file.write("<!------!>\n\n")
     my_file.close()
-    print("<!------!>")
-    from datetime import datetime
-    print(datetime.now())
-    print("         ^i            ^b {0} {1} (id = {2}) \n {3}".format(message.from_user.first_name,
-                                                              message.from_user.last_name,
-                                                              str(message.from_user.id), message.text))
 
 
 bot.polling()
-
